c85e97c4c910718b80ccf7590a891ff9b9ccf320
FlightVin <vineeth.bhat@gmail.com>
2024-02-19 00:36:35 +0530

    Add functionality for running checkstyle and downloading github repo

--- utilities.py
+++ utilities.py
@@ -1,5 +1,28 @@
 import requests
 import json
+import subprocess
+import os
+
+def download_github_repo_wget(repo_url, destination_path):
+    """
+    Download a GitHub repository as a ZIP archive using wget and extract it to a specified destination path.
+
+    Args:
+        repo_url (str): GitHub repository URL.
+        destination_path (str): Local path to store the downloaded repository.
+    """
+    # Use wget to download the ZIP archive
+    subprocess.run(["wget", repo_url + "/archive/main.zip", "-O", "temp.zip"])
+
+    # Create the destination directory if it doesn't exist
+    os.makedirs(destination_path, exist_ok=True)
+
+    # Extract the ZIP archive to the destination path
+    subprocess.run(["unzip", "temp.zip", "-d", destination_path])
+
+    # Remove the temporary ZIP file
+    os.remove("temp.zip")
+
 
 def get_github_token(json_file_path) -> str:
     """
@@ -52,3 +75,39 @@ def create_pull_request(repo_owner, repo_name, base_branch, head_branch, title, 
         print(f"Failed to create pull request. Status code: {response.status_code}")
         print(response.text)
         return None
+    
+def run_checkstyle(checkstyle_dir_path, directory_path, output_file):
+    """
+    Run Checkstyle on Java code in a directory and save the results to an output file.
+
+    Args:
+        checkstyle_dir_path (str): Path to the Checkstyle directory.
+        directory_path (str): Directory containing Java source files.
+        output_file (str): File to save Checkstyle results.
+    
+    Returns:
+        CompletedProcess: The result of the Checkstyle command.
+    """
+    checkstyle_jar_path = os.path.join(checkstyle_dir_path, "checkstyle-10.13.0-all.jar")
+    checkstyle_xml_path = os.path.join(checkstyle_dir_path, "metric-checks.xml")
+
+    command = f"java -jar {checkstyle_jar_path} \
+        -c {checkstyle_xml_path} {directory_path} \
+        > {output_file}"
+    
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+    # processing output even further
+    with open(output_file, 'r') as file:
+        lines = file.readlines()
+
+    # removing system dependent paths
+    prefix_to_remove = directory_path
+    modified_lines = [line.split(prefix_to_remove)[-1].strip()+"\n" for line in lines]
+    modified_lines = modified_lines[1:-1] # leave out audit statements
+
+    with open(output_file, 'w') as file:
+        file.writelines(modified_lines)
+
+    return result
+
